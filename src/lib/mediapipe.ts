5341273a239910783d30289f21effd6da022cfb4
Lina Aggarwal <linaaggarwal03@gmail.com>
2026-07-24 21:49:02 +0530

    feat: initialize MediaPipe architecture

---
+++ src/lib/mediapipe.ts
@@ -0,0 +1,21 @@
+import {
+  FilesetResolver,
+  HandLandmarker,
+} from "@mediapipe/tasks-vision";
+
+export async function createHandLandmarker() {
+  const vision = await FilesetResolver.forVisionTasks(
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm"
+  );
+  const handLandmarker = await HandLandmarker.createFromOptions(
+  vision,
+  {
+    baseOptions: {
+      modelAssetPath:
+        "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task",
+    },
+    runningMode: "VIDEO",
+    numHands: 2,
+  }
+);
+}
